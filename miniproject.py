c287315ccf9435db8b80f94f42580c27a9b85a6d
ujjwal0902 <119126307+ujjwal0902@users.noreply.github.com>
2022-11-30 09:51:00 +0530

    Add files via upload

---
+++ miniproject.py
@@ -0,0 +1,7 @@
+# Python program to calculate how many days person survived in this world
+from datetime import date
+
+day1  = date.today()             #CURRENT DATE
+day2 = date(2005, 1, 5)          #ENTER BIRTH DATE HERE (yyyy,mm,dd)
+diff = day1 - day2
+print(diff.days)
